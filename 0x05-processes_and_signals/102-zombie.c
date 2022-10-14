cdc247c456741a7cbf228122474294c47518889f
Edmund <60813896+EdmondKoech019@users.noreply.github.com>
2022-10-14 10:41:12 -0700

    Add files via upload

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 /**
- * infinite_while - creates infinite loop to make program hang
- * Return: 0
+ * main - zombies
+ *
+ * Description: make five zombies
+ * Return: 0 for success
  */
-
-int infinite_while(void)
-{
-	while (1)
-	{
-		sleep(1);
-	}
-	return (0);
-}
-
-/**
- * main - creates 5 zombie processes
- * Return: 0 on success
- */
-
 int main(void)
 {
 	int i;
-	pid_t z_pid;
+	pid_t pidme;
 
-	for (i = 0; i < 5; i++)
+	i = 0;
+	while (i < 5)
 	{
-		z_pid = fork();
-		if (!z_pid)
-			return (0);
-		printf("Zombie process created, PID: %d\n", z_pid);
+		pidme = fork();
+		if (pidme)
+			printf("Zombie process created, PID: %i\n", pidme);
+		else
+			exit(0);
+		i++;
 	}
-
-	infinite_while();
+	sleep(100);
 	return (0);
 }
